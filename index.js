f630fc95313ac9adccfe75f418aae1fc8254a740
mayconbj15 <mayconbj15@gmail.com>
2019-06-24 21:26:46 -0300

    Commit para teste de evento de load na web

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+var teste = window.document.querySelector('div.welcome').addEventListener("load", showButton)
+
+
 var control = document.querySelector('div.welcome')
 //var button = document.querySelector('')
 
 var button = document.querySelector('button').style.display = 'none'
-transition() //função que é chamada sempre que o site é aberto
+//transition() //função que é chamada sempre que o site é aberto
 
 function transition(){
     control.classList.add('welcome-transition')
@@ -10,7 +13,10 @@ function transition(){
 
 function showButton(){
     var button = document.querySelector('button').style.display = 'inline'
+    console.log('button')
 }
 
 control.addEventListener('transitionend', showButton)
+
+
 
