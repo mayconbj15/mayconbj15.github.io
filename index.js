bb0272b7347f41dcc3ec59ceb9a3d7e52dc4b3e9
mayconbj15 <mayconbj15@gmail.com>
2019-06-25 23:45:02 -0300

    Modificando página inicial e header

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
 /**
  * Declarações de variáveis globais e inicializações
  */
+
 var control = document.querySelector('div.welcome')
 
 var button = document.querySelector('button').style.display = 'none'
 
-
 /**
  * Funções
  */
+
 function transition(){
     control.classList.add('welcome-transition')
 }
 
 function showButton(){
     var button = document.querySelector('button').style.display = 'inline'
+
 }
 
+function showHeader(){
+    header.style.display = 'inline-block'
+    header.style.textAlign = 'center'
+}
 
 /**
  * Eventos
  */
 
 window.addEventListener('load', transition)
-
-control.addEventListener('transitionend', showButton)
 
 
 /**
