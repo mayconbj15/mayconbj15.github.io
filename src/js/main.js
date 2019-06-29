3b27fd50622d9c8bb9e0f69afa15825ca686defd
mayconbj15 <mayconbj15@gmail.com>
2019-06-29 02:05:44 -0300

    Começando script de animação do header

---
+++ src/js/main.js
@@ -0,0 +1,26 @@
+/**
+ * Script de animação do header
+ */
+
+ var imagens = document.querySelectorAll('header img')
+ 
+
+ imagens.forEach(function(element) {
+     element.addEventListener('mouseenter', () => {maxHeight(element)})
+ });
+
+
+ imagens.forEach(function(element) {
+     element.addEventListener('mouseout', () => {minHeight(element)})
+ });
+
+
+ function maxHeight(element){
+    console.log('max')
+    element.height += 5;
+ }
+
+ function minHeight(element){
+    console.log('min')
+    element.height -= 5;
+ }
